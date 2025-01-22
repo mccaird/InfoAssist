5c2384f511a72e7507d868326e85a979cd0f4c06
mccaird <140186166+mccaird@users.noreply.github.com>
2025-01-22 17:59:17 +0000

    Fixed-Allow tf resource delettion in providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -25,7 +25,7 @@ provider "azurerm" {
     key_vault {
       purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = true
-    },
+    }
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
